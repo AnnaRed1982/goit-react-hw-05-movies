c8caa3ee9b3ec893a481b74c880e3edbe1e6c13c
annared <annared1982@gmail.com>
2023-03-14 17:32:03 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        {/* <Route index element={<Home />} /> */}
         <Route path="movies" element={<Movies />} />
       </Route>
     </Routes>
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export const Movies = () => {
+const Movies = () => {
   useEffect(() => {
     //http request
   }, []);
