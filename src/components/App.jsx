f39ab5e51908e108ee920931beeeb86c90196614
annared <annared1982@gmail.com>
2023-03-14 17:19:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
-// import Home from '../pages/Home';
-// import Movies from '../pages/Movies';
-// import { Route, Routes } from 'react-router-dom';
-// import { Layout } from './Layout';
+import Home from '../pages/Home';
+import Movies from '../pages/Movies';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
 
 export const App = () => {
-  // return (
-  //   <Routes>
-  //     <Route path="/" element={<Layout />}>
-  //       <Route index element={<Home />} />
-  //       <Route path="/movies" element={<Movies />}></Route>
-  //     </Route>
-  //   </Routes>
-  // );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+      </Route>
+    </Routes>
+  );
 };
