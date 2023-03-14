4b561fefeed5d4e68f424a70b68fd5bb1b9ac0fa
annared <annared1982@gmail.com>
2023-03-14 17:33:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import Home from '../pages/Home';
+// import Home from '../pages/Home';
 import Movies from '../pages/Movies';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
