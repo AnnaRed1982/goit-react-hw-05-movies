2664ccf0055bae03d7fab53c2bfc799b046090c9
annared <annared1982@gmail.com>
2023-03-15 15:23:02 +0200

    Update MovieDetails.js

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,6 +4,7 @@ import { fetchMovieId } from '../services/api';
 
 const MovieDetails = () => {
   const POSTER_PATH = 'https://image.tmdb.org/t/p/w500';
+
   const { movieId } = useParams();
 
   const [movie, setMovie] = useState([]);
@@ -27,15 +28,22 @@ const MovieDetails = () => {
     };
     fetchMovie();
   }, [movieId]);
-
-  return (
-    <>
-      <button type="button">Go back</button>
-      <img src={`${POSTER_PATH}${movie.poster_path}`} alt="movie poster" />
-      <h2>{movie.title}</h2>
-      <p>({movie.release_date.slice(0, 4)})</p>
-    </>
-  );
+  if (status === 'pending') {
+    return <h2>Loading...</h2>;
+  }
+  if (status === 'rejected') {
+    return <h2>Whoops, something went wrong: {error}</h2>;
+  }
+  if (status === 'resolved') {
+    return (
+      <>
+        <button type="button">Go back</button>
+        <img src={`${POSTER_PATH}${movie.poster_path}`} alt="movie poster" />
+        <h2>{movie.title}</h2>
+        <p>{movie.release_date.slice(0, 4)}</p>
+      </>
+    );
+  }
 };
 
 export default MovieDetails;
